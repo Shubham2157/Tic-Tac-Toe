8ef620678baa45ea55498f30eb3a8e782aee48ed
Shubham2157 <shubhamkrjha99@gmail.com>
2020-09-19 14:06:32 +0530

    Updates

---
+++ precache-manifest.53d4120102aca5ca95d5ba2fe5c664c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2a6274a164b1611481d7f281bb8b55",
+    "url": "/Contactme/index.html"
+  },
+  {
+    "revision": "0a290f53a519be47bcb3",
+    "url": "/Contactme/static/css/main.79d71633.chunk.css"
+  },
+  {
+    "revision": "0c03bcbbd938e488ff4c",
+    "url": "/Contactme/static/js/2.33ef16d9.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/Contactme/static/js/2.33ef16d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a290f53a519be47bcb3",
+    "url": "/Contactme/static/js/main.b57fe438.chunk.js"
+  },
+  {
+    "revision": "6dbbc47d89e71bd99d2f",
+    "url": "/Contactme/static/js/runtime-main.36ad7e13.js"
+  }
+]);
